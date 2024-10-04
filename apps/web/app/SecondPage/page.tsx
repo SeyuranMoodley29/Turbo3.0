a53c7fa5c9b7d08bd2984cead9d33c730d6007b6
Seyuran Moodley <smoodley@retrorabbit.co.za>
2024-10-04 09:00:17 +0200

    added button to page 2

--- apps/web/app/SecondPage/page.tsx
+++ apps/web/app/SecondPage/page.tsx
@@ -1,11 +1,17 @@
-// apps/web/app/second_page.tsx
+import Link from "next/link";
 
 export default function SecondPage() {
     return (
       <div>
         <h1>This is the Second Page</h1>
-        <p>Please dont judge me.</p>
+        <p>Please don't judge me.</p>
+        
+        {/* Button to navigate to another page */}
+        <Link href="/">
+        <button style={{ border: '2px solid #007BFF', padding: '10px 20px', borderRadius: '5px' }}>
+            Go to Another Page
+          </button>
+        </Link>
       </div>
     );
-  }
-  
+}
